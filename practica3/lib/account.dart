b43904408c7510010071909e2f7880db256e6e5e
ZyrearUni <zyreargonzalez@correo.ugr.es>
2025-05-09 18:05:07 +0200

    Practica3 added and base readied

---
+++ practica3/lib/account.dart
@@ -0,0 +1,15 @@
+abstract class Transaction {
+
+}
+
+class DepositTransaction extends Transaction {
+
+}
+
+class WithdrawTransaction extends Transaction {
+
+}
+
+class DepositTransaction extends Transaction {
+  String to_account = "";
+}
